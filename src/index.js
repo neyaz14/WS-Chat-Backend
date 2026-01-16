cca36d189b20f6f5aea469eda225858fd57d1101
Neyaz Morshid <neyazmorshid@gmail.com>
2026-01-16 17:22:15 +0600

    all new

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ wss.on('connection', function connection(ws) {
         wss.clients.forEach(function each(client) {
             if (client != ws && client.readyState === WebSocket.OPEN) {
                 client.send(data, { binary: isBinary });
-                console.log(data);
+                // console.log(data);
             }
         });
     });
